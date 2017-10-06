728af0a0c1287b570204c01590f0a279c7b4dbad
Neil Brownell <neil@onnit.com>
2017-10-06 02:31:12 -0500

    expanding to rock paper scissors lizard spock

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -11,35 +11,63 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // create a sanitized array of the two hands
-  const handArr = [hand1, hand2].map((n) => n.toLowerCase().replace(/\s/g, ''));
+  // create an array of the two hands for easier manipulation
+  let handArr = [hand1, hand2];
 
-  // if hand 1 and hand 2 are the same, we can skip the rest of the logic
-  if (handArr[0] === handArr[1]) return 'It\'s a tie!';
+  // declare an array of accepted hand values
+  const validHands = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  
+  // attempt to sanitize the inputs and check if they are valid hands
+  for (let value of handArr) {
+    value = value.toLowerCase().replace(/\s/g, '');
+    if (!validHands.includes(value)) {
+      return 'Invalid input... Accepted values are ' + validHands.join(', ');
+    }
+  }
 
-  // if it's not a tie, then see who won
-  // start by defining the winning scenarios for hand 1
+  // if hand 1 and hand 2 are the same, it's a tie, we're done, return early
+  if (handArr[0] === handArr[1]) {
+    return 'It\'s a tie!';
+  }
+
+  // if it's not a tie, let's start by defining the scenarios where hand 1 wins
+  // let's also make a note of the verb that goes with each scenario
+  // then we can log an explanation to the console
   const hand1Wins = [
-    ['paper', 'rock'],
-    ['rock', 'scissors'],
-    ['scissors', 'paper']
+    {scenario: ['paper',    'rock'],     verb: 'covers'     },
+    {scenario: ['rock',     'scissors'], verb: 'crushes'    },
+    {scenario: ['scissors', 'paper'],    verb: 'cut'        },
+    {scenario: ['rock',     'lizard'],   verb: 'crushes'    },
+    {scenario: ['lizard',   'spock'],    verb: 'poisons'    },
+    {scenario: ['spock',    'scissors'], verb: 'smashes'    },
+    {scenario: ['scissors', 'lizard'],   verb: 'decapitate' },
+    {scenario: ['lizard',   'paper'],    verb: 'eats'       },
+    {scenario: ['paper',    'spock'],    verb: 'disproves'  },
+    {scenario: ['spock',    'rock'],     verb: 'vaporizes'  }
   ];
+  
 
-  // the winning scenarios for hand 2 are the same as hand 1, but reversed
-  // use JSON parsing hack to deep-copy hand 1 and flip-flop the arrays within
-  const hand2Wins = JSON.parse(JSON.stringify(hand1Wins));
-  hand2Wins.map((n) => n.reverse());
+  // the scenarios where hand 2 wins are going to be the same as hand 1
+  // but with rock/paper flipped to paper/rock, etc.
+  let hand2Wins = JSON.parse(JSON.stringify(hand1Wins));
+  for (let win of hand2Wins) {
+    win.scenario = win.scenario.reverse();
+  }
 
-  // check our hand array against each of the winning hand 1 scenarios
-  for (let scenario of hand1Wins) {
-    if (scenario.join() === handArr.join()) {
+  // check each hand1Wins scenario to see if it applies to the current game
+  for (let win of hand1Wins) {
+    // join the arrays to strings to compare them more easily
+    if (win.scenario.join() === handArr.join()) {
+      console.log(handArr[0] + ' ' + win.verb + ' ' + handArr[1]);
       return 'Hand one wins!';
     }
   }
 
-  // do the same check but for the hand 2 scenarios
-  for (let scenario of hand2Wins) {
-    if (scenario.join() === handArr.join()) {
+  // check each hand2Wins scenario to see if it applies to the current game
+  for (let win of hand2Wins) {
+    // join the arrays to strings to compare them more easily
+    if (win.scenario.join() === handArr.join()) {
+      console.log(handArr[1] + ' ' + win.verb + ' ' + handArr[0]);
       return 'Hand two wins!';
     }
   }
